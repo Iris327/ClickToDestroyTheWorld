df2a9ba14743bf0ff8da9c1c41be56bf806c942a
IrisPeng327 <iristpeng@gmail.com>
2022-01-28 16:11:59 -0800

    Working on upgrade button

--- app/src/main/java/com/example/clicktodestroytheworld/MainActivity.kt
+++ app/src/main/java/com/example/clicktodestroytheworld/MainActivity.kt
@@ -14,6 +14,16 @@ class MainActivity : AppCompatActivity() {
         //variable declarations
         val incrementBtn = findViewById<Button>(R.id.incrementButton)
         val scoreText = findViewById<TextView>(R.id.playerScore)
+        val upgradeBtn = findViewById<Button>(R.id.upgradeButton)
+        var storyText = findViewById<TextView>(R.id.storyText)
+
+        //set story text margins
+        /*val params = LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,LinearLayout.LayoutParams.WRAP_CONTENT)
+        params.setMargins( 10, 10, 10, 10)
+        storyText.layoutParams = params
+        */
+        //disable upgradeBtn until score reaches certain value
+        upgradeBtn.isEnabled = false
 
         //increments the player score after increment score button is clicked
         incrementBtn?.setOnClickListener() {
